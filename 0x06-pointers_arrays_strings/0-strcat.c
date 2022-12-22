dca599b96e3851a5a6c298ccea15f63a1a399782
Yordiemma <yordisweer56ggmail.com>
2022-12-22 14:59:48 -0800

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * _strcat - contantes two strings
- * dest: input value
- * src: input value
+ * @dest: input value
+ * @src: input value
  *
  * Return: void
  */
@@ -11,7 +11,7 @@ char *_strcat(char *dest, char *src)
 	int i;
 	int j;
 
-	i - 0;
+	i = 0;
 	while (dest[i] != '\0')
 	{
 		i++;
