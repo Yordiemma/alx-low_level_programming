ffa4b1c657ab6a2baf5c1cef78663a1a945620f3
Yordiemma <yordisweer56ggmail.com>
2022-12-27 15:31:40 -0800

    I can't fall asleep

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase
+ *
+ * Return: prints alha in lowercase
+ *
+ * Return: Always: 0
+ */
+int main(void)
+{
+
+	char lc;
+
+	for (lc = 'a'; letter <= 'z'; lc++)
+		putchar(lc);
+
+	putchar('\n');
+
+	return (0);
+}
+
