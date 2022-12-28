7f9e7e325c412b08b31abef530a674ec159ddb7a
Yordiemma <yordisweer56ggmail.com>
2022-12-27 17:08:51 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print singel digeits in bash 10 using putchar
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+	putchar('\n');
+
+	return (0);
+}
