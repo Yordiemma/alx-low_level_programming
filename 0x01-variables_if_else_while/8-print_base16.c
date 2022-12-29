d100984f40d1770dfdce474864dfda4d308ea400
Yordiemma <yordisweer56ggmail.com>
2022-12-29 15:22:15 -0800

    decimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,7 +12,8 @@ int main(void)
 
 for (num = 0; num < 10; num++)
 	putchar((num % 10) + '0');
-for (letter = 'a'; letter <= 'f'; letter++)
+for (le = 'a'; le <= 'f'; le++)
+	putchar(le);
 putchar('\n');
 
 	return (0);
